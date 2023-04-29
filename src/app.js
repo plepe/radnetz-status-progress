2fc7ab3bb979406b6ed636b1545590d5385a312c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-04-29 22:46:55 +0200

    Show progression

--- src/app.js
+++ src/app.js
@@ -1,28 +1,55 @@
+import async from 'async'
 import Chart from 'chart.js/auto'
 import getResult from './getResult'
+import getStatuses from './getStatuses'
+
+let statuses
 
 window.onload = () => {
-  getResult('2022', (err, result) => {
-    console.log(result)
+  async.parallel({
+    statuses: (done) => getStatuses(done),
+    result: (done) => getResult('2022', done)
+  }, (err, data) => {
+    if (err) { return console.error(err) }
+    show(data)
+  })
+}
+
+function show ({ statuses, result }) {
+    const datasets = statuses.map(status => {
+      return {
+        label: status.Status,
+        backgroundColor: status.Farbe,
+        borderColor: status.Farbe,
+        stepped: true,
+        fill: true,
+        data: Object.entries(result).map(l => {
+          return {
+            x: l[0],
+            y: l[1].filter(v => v === status.Status).length
+          }
+        }),
+        borderWidth: 1
+      }
+    })
+    console.log(datasets)
 
     const ctx = document.getElementById('chart')
     new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
         labels: Object.keys(result),
-        datasets: [{
-          label: 'Anzahl Bauprojekte gesamt',
-          data: Object.values(result).map(l => l.length),
-          borderWidth: 1
-        }]
+        datasets
       },
       options: {
         scales: {
+          x: {
+          },
           y: {
+            stacked: true,
             beginAtZero: true
           }
         }
       }
     })
-  })
 }
---
+++ src/getStatuses.js
@@ -0,0 +1,25 @@
+const csvtojson = require('csvtojson')
+
+const config = require('../config.json')
+
+let cache = null
+
+module.exports = function getStatuses (callback) {
+  if (cache) {
+    return callback(null, cache)
+  }
+
+  fetch(config.urlStatues)
+    .then(req => req.text())
+    .then(body => {
+      const data = []
+
+      csvtojson({})
+        .fromString(body)
+        .subscribe(line => data.push(line))
+        .on('done', () => {
+          cache = data
+          callback(null, data)
+        })
+    })
+}
