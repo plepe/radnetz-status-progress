0ef96a4b7f46166ca6b586311257678fb5a02a76
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-04-29 21:02:28 +0200

    Show real values

--- src/app.js
+++ src/app.js
@@ -4,25 +4,25 @@ import getResult from './getResult'
 window.onload = () => {
   getResult('2022', (err, result) => {
     console.log(result)
-  })
 
-  const ctx = document.getElementById('chart')
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    const ctx = document.getElementById('chart')
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: Object.keys(result),
+        datasets: [{
+          label: 'Anzahl Bauprojekte gesamt',
+          data: Object.values(result).map(l => l.length),
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
         }
       }
-    }
+    })
   })
 }
